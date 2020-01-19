23730586a8983af2fe9ba249dfc743326e7f0bde
Petro  Gomes <petrogomes@camilles-mbp.speedport_w_723v_1_47_000>
2020-01-19 17:36:31 +0100

    Fix timezone (II)

--- src/components/__mocks__/globals/date.js
+++ src/components/__mocks__/globals/date.js
@@ -1,7 +1,4 @@
 import MockDate from 'mockdate';
 
-// Mock Timezone
-process.env.TZ = 'Europe/Berlin';
-
 // Mock Date
-MockDate.set(new Date('2019-10-29T03:24:00'));
+MockDate.set(new Date('2019-10-29T03:24:00'), 0);
