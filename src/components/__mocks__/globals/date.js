ad6dd19acc59586c76be48ba80acd37299f8e535
Petro  Gomes <petrogomes@Camilles-MBP.localdomain>
2020-01-20 12:28:51 +0100

    Revert solution for timezone mock

--- src/components/__mocks__/globals/date.js
+++ src/components/__mocks__/globals/date.js
@@ -2,8 +2,3 @@ import MockDate from 'mockdate';
 
 // Mock Date
 MockDate.set(new Date('2019-10-29T03:24:00'), 0);
-
-// Mock Timezone
-Date.prototype.toString = function() {
-  return this.toISOString();
-};
